d2d2984dd3791df0035fdb90ea7a60c8a625254a
KwanjiChoi <kwanji0912@gmail.com>
2021-03-03 15:42:54 +0900

    add post query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,19 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-    field :posts, Types::PostType, null: false
+    field :posts, [Types::PostType], null: false
     def posts
       Post.all
     end
 
 
-    filed :post, Types::Posttype, null: false do
-      argument :id, Int, required: false
+    field :post, Types::PostType, null: false do
+      description 'gets post informations'
+      argument :id, Int, required: false, description: 'post id'
     end
     def post(id:)
       Post.find(id)
